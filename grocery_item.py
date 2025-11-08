b1eb43c4da1ae72856faea99653d15ce9acef6f1
Gor <gordeykurakin@gmail.com>
2025-11-08 15:30:25 +0100

    improved the grocery_item class

--- grocery_item.py
+++ grocery_item.py
@@ -7,3 +7,11 @@ class GroceryItem:
     category: str
     price: float
     macros: str
+    days_to_expire: int
+
+    def is_expiring_soon(self, threshold=3):
+        """Check if the item will expire within a given number of days."""
+        return self.days_to_expire <= threshold
+
+    def __repr__(self):
+        return f"<GroceryItem {self.item_name} (${self.price}), {self.days_to_expire}d to expire>"
